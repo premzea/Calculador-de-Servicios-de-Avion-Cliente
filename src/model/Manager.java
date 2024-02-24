701a4fbbb011aa413edd7c3d9f08700194e5b918
premzea <prem.zea@u.icesi.co>
2024-02-24 12:33:32 -0500

    version 2

--- src/model/Manager.java
+++ src/model/Manager.java
@@ -3,27 +3,69 @@ import java.util.Scanner;
 public class Manager{
 
     public static void main(String[] x){
-        double LugggageWeight;
-        int TravelType, NumberofPassengers;
+        int LugggageWeight;
+        int TravelType, NumberofPassengers, recommendationPrice;
+        String SelectedPackage;
+
+        // data
+        int price10kg = 50000;
+        int price23kg = 100000; 
+
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("Choose a travel type between travelling alone (1) or travelling with friends (2): ")
-        TravelType = sc.getInt();
-        System.out.println("Luggage Weight: ");
-        LugggageWeight = sc.getDouble();
+        System.out.println("Choose a travel type between travelling alone (1) or travelling with friends (2): ");
+        TravelType = sc.nextInt();
+        System.out.println("Luggage Weight (Kg): ");
+        LugggageWeight = sc.nextDouble();
         NumberofPassengers = TravelType == 1 ? 1:3;
-
+        Integer[] PackagePrices = new Integer[3];
+        PackagePrices = PackageCalculations();
+        System.out.println(PackagePrices);
+        String recommendation;
         if (NumberofPassengers == 1) {
+            if (LugggageWeight == 3 || LugggageWeight == 13 || LugggageWeight == 36){
+                if (LugggageWeight == 3){
+                    recommendation = "XS Package";
+                } else if (LugggageWeight == 13){
+                    recommendation = "S Package";
+                } else if (LugggageWeight == 36){
+                    recommendation = "M Package";
+                }
+            } else if (LugggageWeight > 3 && LugggageWeight < 36){
+                if (LugggageWeight > 36 - )
+                for (i = 0; i < 3; i++){
+                    int weight = LugggageWeight;
+                    weight = LugggageWeight - 3 - 10*i;
+                    if (weight = 0){
+                        break;
+                    }
+                }
+                recommendation = "XS Package with an addition of " + i + "10kg bags";
+                recommendationPrice = PackagePrices[0] + i*price10kg;
+            }
+            } else if (LugggageWeight > 13 && LugggageWeight < 36){
+                for (i = 0; i < 3; i++){
+                    int weight = LugggageWeight;
+                    weight = LugggageWeight - 3 - 10*i;
+                    if (weight = 0){
+                        break;
+                    }
 
+
+
+            System.out.println("We recommend you purchase the " + recommendation + "so you can take all your luggage, for a price of " + recommendationPrice);
+            System.out.println("which package are you purchasing? \n");
+            SelectedPackage = sc.nextString();
         }
         else if (NumberofPassengers == 3){
-            String [] Passengers = [];
-            for (int i = 1; i < NumberofPassengers; i++){
-                System.out.println("Well do Passengers" + i + "package calculations \n");
-                System.out.println("What's your name? ")
-                String Passenger = sc.nextString();
-                Passengers.add(Passenger);
-                Integer[] PackagePrices = PackageCalculations();
+            String[] Passengers = new String[3];
+            
+            for (int i = 0; i < NumberofPassengers; i++){
+                System.out.println("Well do Passengers" + (i+1) + "package calculations \n");
+                System.out.println("What's your name? ");
+                String Passenger = sc.next();
+                Passengers[i] = (Passenger);
+                
             }
         }
 
@@ -31,13 +73,13 @@ public class Manager{
 
     public static Integer[] PackageCalculations(){
         Integer XSPackagePrice, SPackagePrice, MPackagePrice;
-        List <Integer> PackagePrices = [];
+        Integer[] PackagePrices = new Integer[3];
         XSPackagePrice = 175000;
-        PackagePrices.add(XSPackagePrice);
+        PackagePrices[0] = XSPackagePrice;
         SPackagePrice = (Integer) XSPackagePrice + (XSPackagePrice * 0.25);
-        PackagePrices.add(SPackagePrice);
+        PackagePrices[1]= SPackagePrice;
         MPackagePrice = (Integer) SPackagePrice + (SPackagePrice * 0.30);
-        PackagePrices.add(MPackagePrice);
+        PackagePrices[2] = MPackagePrice;
         
     }
 
